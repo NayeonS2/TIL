76ea6e67ebda98bfed55b7aa551c71ef5850faa7
NayeonS2 <k101613@gmail.com>
2022-11-07 18:03:56 +0900

    update

---
+++ Vue/05_vue/todo-vuex-app/src/components/TodoForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input 
+    type="text"
+    v-model="todoTitle"
+    @keyup.enter="createTodo"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoForm',
+    data() {
+        return {
+            todoTitle: null,
+        }
+    },
+    methods: {
+        createTodo() {
+            // console.log(this.todoTitle)
+            this.$store.dispatch('createTodo', this.todoTitle)
+            this.todoTitle = null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
