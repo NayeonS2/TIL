99ee44d766c5e676f8e30f583090f136f92e1170
NayeonS2 <k101613@gmail.com>
2022-11-12 23:55:32 +0900

    update

--- Problem Solving/[SWEA]_5658.py
+++ Problem Solving/[SWEA]_5658.py
@@ -1,16 +1,27 @@
+# 보물상자 비밀번호
+
 import sys
 sys.stdin = open('input.txt')
 
+
 T = int(input())
+for tc in range(1,T+1):
+    N,K = map(int,input().split())
+    nums = list(input())
 
-for tc in range(1,1+T):
-    N, K = map(int,input().split())
-    nums = input()
-    rot = N//4
-    nums += nums[:rot]
+    turns = N//4
+    res = []
+    for _ in range(turns):
+        for i in range(0,len(nums),turns):
+            tmp = int(''.join(nums[i:i+turns]),16)
+            if tmp not in res:
+                res.append(tmp)
+        nums = [nums[-1]] + nums[:-1]
 
-    poss = []
-    for i in range(len(nums)-2):
-        if int(nums[i:i+rot],16) not in poss:
-            poss.append(int(nums[i:i+rot],16))
-    print(f'#{tc} {sorted(poss,reverse=True)[K-1]}')
+
+
+    res = sorted(res,reverse=True)
+
+    print(f'#{tc} {res[K-1]}')
+
+
