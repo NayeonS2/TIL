441297b205095f79e84e50c1ea4dbb5781505c92
NayeonS2 <k101613@gmail.com>
2022-11-27 17:08:04 +0900

    update

--- Problem Solving/[ECT]_Implementation_08.py
+++ Problem Solving/[ECT]_Implementation_08.py
@@ -1,2 +1,58 @@
 # 프로그래머스 기둥과 보 설치
+# 이중배열, deepcopy 이용했으나 실패
 
+# 기둥은 바닥 위에 있거나 보의 한쪽 끝 부분 위에 있거나, 또는 다른 기둥 위에 있어야 합니다.
+# 보는 한쪽 끝 부분이 기둥 위에 있거나, 또는 양쪽 끝 부분이 다른 보와 동시에 연결되어 있어야 합니다.
+# 구조물은 교차점 좌표를 기준으로 보는 오른쪽, 기둥은 위쪽 방향으로 설치 또는 삭제
+
+# build_frame의 원소는 [x, y, a, b]형태
+# x, y는 기둥, 보를 설치 또는 삭제할 교차점의 좌표이며, [가로 좌표, 세로 좌표] 형태입니다.
+# a는 설치 또는 삭제할 구조물의 종류를 나타내며, 0은 기둥, 1은 보를 나타냅니다.
+# b는 구조물을 설치할 지, 혹은 삭제할 지를 나타내며 0은 삭제, 1은 설치를 나타냅니다.
+# return 하는 배열은 가로(열) 길이가 3인 2차원 배열로, 각 구조물의 좌표를 담고있어야 합니다.
+# return 하는 배열의 원소는 [x, y, a] 형식입니다.
+# x, y는 기둥, 보의 교차점 좌표이며, [가로 좌표, 세로 좌표] 형태입니다.
+# return 하는 배열은 x좌표 기준으로 오름차순 정렬하며, x좌표가 같을 경우 y좌표 기준으로 오름차순 정렬해주세요.
+# x, y좌표가 모두 같은 경우 기둥이 보보다 앞에 오면 됩니다.
+
+
+# 기둥 2 / 보 3
+n = 5
+build_frame = [[0, 0, 0, 1], [2, 0, 0, 1], [4, 0, 0, 1], [0, 1, 1, 1], [1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1],
+               [2, 0, 0, 0], [1, 1, 1, 0], [2, 2, 0, 1]]
+
+
+def check(answer):
+    for x, y, a in answer:
+        if a == 0:
+            if y == 0 or [x - 1, y, 1] in answer or [x, y, 1] in answer or [x, y - 1, 0] in answer:
+                continue
+            else:
+                return False
+        if a == 1:
+            if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or (
+                    [x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
+                continue
+            else:
+                return False
+    return True
+
+def solution(n, build_frame):
+    answer = []
+    for info in build_frame:
+        x, y, a, b = info
+
+        if b == 0:
+            answer.remove([x, y, a])
+            if not check(answer):
+                answer.append([x, y, a])
+
+        if b == 1:
+            answer.append([x, y, a])
+            if not check(answer):
+                answer.remove([x, y, a])
+
+    answer = sorted(answer, key=lambda x: (x[0], x[1], x[2]))
+    return answer
+
+print(solution(n,build_frame))
--- Problem Solving/prac.py
+++ Problem Solving/prac.py
@@ -1,9 +1,66 @@
-# 벽돌깨기
+# 기둥 2 / 보 3
+n = 5
+build_frame = [[0, 0, 0, 1], [2, 0, 0, 1], [4, 0, 0, 1], [0, 1, 1, 1], [1, 1, 1, 1], [2, 1, 1, 1], [3, 1, 1, 1],
+               [2, 0, 0, 0], [1, 1, 1, 0], [2, 2, 0, 1]]
 
-T = int(input())
+from copy import deepcopy
 
-for tc in range(1,T+1):
-    N, W, H = map(int,input().split())
+def check(n,arr):
+    for i in range(n + 1):
+        for j in range(n + 1):
+            if arr[i][j] == 2:
+                if i == n or arr[i][j - 1] == 3 or arr[i][j] == 3 or arr[i + 1][j] == 2:
+                    continue
+                else:
+                    return False
+            if arr[i][j] == 3:
+                if arr[i + 1][j] == 2 or arr[i + 1][j + 1] == 2 or (arr[i][j - 1] == 3 and arr[i][j + 1] == 3):
+                    continue
+                else:
+                    return False
+    return True
+
+
+arr = [[0] * (n + 1) for _ in range(n + 1)]
+
+for info in build_frame:
+    x, y, a, b = info
+    i, j = n - y, x
+
+    tmp = deepcopy(arr)
+    if a == 0:
+        if b == 0:
+            tmp[i][j] = 0
+            if check(n,tmp):
+                arr = tmp
+            else:
+                tmp[i][j] = arr[i][j]
+        if b == 1:
+            if i == n or arr[i][j - 1] == 3 or arr[i][j] == 3 or arr[i + 1][j] == 2:
+                arr[i][j] = 2
+
+    if a == 1:
+        if b == 0:
+            tmp[i][j] = 0
+            if check(n,tmp):
+                arr = tmp
+            else:
+                tmp[i][j] = arr[i][j]
+        if b == 1:
+            if arr[i + 1][j] == 2 or arr[i + 1][j + 1] == 2 or (arr[i][j - 1] == 3 and arr[i][j + 1] == 3):
+                arr[i][j] = 3
+
+answer = []
+for i in range(n + 1):
+    for j in range(n + 1):
+        if arr[i][j] == 2:
+            answer.append([j, n - i, 0])
+        if arr[i][j] == 3:
+            answer.append([j, n - i, 1])
+
+answer = sorted(answer, key=lambda x: (x[0], x[1], x[2]))
+
+print(answer)
 
 
 
