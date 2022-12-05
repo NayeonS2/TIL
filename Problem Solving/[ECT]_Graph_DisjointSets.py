edab62bb34dbb55c5417402a30365cd10edea1e7
NayeonS2 <k101613@gmail.com>
2022-12-05 19:49:36 +0900

    update

--- Problem Solving/[ECT]_Graph_DisjointSets.py
+++ Problem Solving/[ECT]_Graph_DisjointSets.py
@@ -8,8 +8,8 @@ sys.stdin = open('input.txt')
 def find_parent(parent, x):
     # 루트노드가 아니라면, 루트 노드를 찾을때까지 재귀적으로 호출
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    return x
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
