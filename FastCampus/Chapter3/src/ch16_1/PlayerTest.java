bd1d18434c059269d27291c6c664ea040e7162aa
NayeonS2 <k101613@gmail.com>
2022-12-15 20:25:06 +0900

    update

---
+++ FastCampus/Chapter3/src/ch16_1/PlayerTest.java
@@ -0,0 +1,20 @@
+package ch16_1;
+
+public class PlayerTest {
+
+	public static void main(String[] args) {
+		
+		Player player = new Player();
+		player.play(1);
+		
+		AdvancedLevel aLevel = new AdvancedLevel();
+		player.upgradeLevel(aLevel);
+		player.play(2);
+		
+		SuperLevel sLevel = new SuperLevel();
+		player.upgradeLevel(sLevel);
+		player.play(3);
+		
+	}
+
+}
