1a8b93f5f644456d1fd4b463efbd7605ab78e217
NayeonS2 <k101613@gmail.com>
2023-01-07 20:06:05 +0900

    ps update

---
+++ Problem Solving/[PGRS]_해시_전화번호목록.py
@@ -0,0 +1,27 @@
+# 전화번호 목록
+
+phone_book = ["12","123","1235","567","88"]
+
+def solution(phone_book):
+    answer = True
+
+    nums = {}
+
+    for phone in phone_book:
+        temp = ''
+        for i in range(len(phone)-1):
+            temp += str(phone[i])
+            nums[temp] = 1
+
+    for phone in phone_book:
+        if phone in nums:
+            answer = False
+    return answer
+
+
+
+print(solution(phone_book))
+
+
+
+
