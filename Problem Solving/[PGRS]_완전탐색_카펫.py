d66890cfcc2efc18b51ce47d1b1408b2798f6db5
NayeonS2 <k101613@gmail.com>
2023-02-20 00:03:49 +0900

    ps update

--- Problem Solving/[PGRS]_완전탐색_카펫.py
+++ Problem Solving/[PGRS]_완전탐색_카펫.py
@@ -3,15 +3,30 @@ yellow = 2
 
 
 def solution(brown, yellow):
-    answer = []
 
-    all_ = brown+yellow
+    def decim(n):
+        tmp = []
+        for i in range(1,n+1):
+            if n%i == 0:
+                tmp.append(i)
+        return tmp
 
-    x = (2*all_ - brown)//8
-    y = all_//x
+    decims = decim(brown+yellow)
 
-    answer = [x,y]
+    if len(decims) % 2 == 0:
+        idx_l = len(decims)//2 - 1
+        idx_r = len(decims) // 2
 
-    return answer
+    elif len(decims) %  2 == 1:
+        idx_l = len(decims)//2
+        idx_r = len(decims)//2
+
+    while True:
+        if (decims[idx_l]-2)*(decims[idx_r]-2) == yellow:
+            return [decims[idx_r],decims[idx_l]]
+        else:
+            idx_l -= 1
+            idx_r += 1
+
 print(solution(brown,yellow))
 
