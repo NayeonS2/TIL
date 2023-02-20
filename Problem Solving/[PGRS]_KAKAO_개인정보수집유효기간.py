76e7eb2e4c6faab48200e947963d035561642a2a
NayeonS2 <k101613@gmail.com>
2023-02-21 01:32:16 +0900

    ps update

--- Problem Solving/[PGRS]_KAKAO_개인정보수집유효기간.py
+++ Problem Solving/[PGRS]_KAKAO_개인정보수집유효기간.py
@@ -1,8 +1,24 @@
-today = "2022.05.19"
-terms = ["A 6", "B 12", "C 3"]
-privacies = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+today = "2020.01.01"
+terms = ["Z 3", "D 5"]
+privacies = ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]
 
+from datetime import datetime
+def time_chk(now,mth):
+    y,m,d = map(int,now.split("."))
 
+    yr = mth//12
+    mn = mth%12
+
+    y -= yr
+
+    if m-mn < 1:
+        y -= 1
+        m = 12-(mn-m)
+    else:
+        m -= mn
+
+    dt = datetime(y ,m, d)
+    return dt
 
 
 def solution(today, terms, privacies):
@@ -13,8 +29,19 @@ def solution(today, terms, privacies):
         alph,num = term.split(" ")
         dic[alph] = num
 
+    chk = dict()
+    for k,v in dic.items():
+        chk[k] = time_chk(today,int(v))
+
+    for i in range(len(privacies)):
+        priv = privacies[i]
+        dat,typ = priv.split(" ")
+        y,m,d = map(int,dat.split("."))
+        print(chk[typ])
+        print(datetime(y,m,d))
+        if (chk[typ] >= datetime(y,m,d)):
+            answer.append(i+1)
 
 
-
-    return dic
+    return answer
 print(solution(today,terms,privacies))
