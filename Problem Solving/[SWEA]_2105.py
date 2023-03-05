1e28052ab236d0e7da50f3b8becb207b57839b54
NayeonS2 <k101613@gmail.com>
2023-03-06 03:15:03 +0900

    ps update

--- Problem Solving/[SWEA]_2105.py
+++ Problem Solving/[SWEA]_2105.py
@@ -7,25 +7,23 @@ sys.stdin = open('input.txt')
 di = [1, 1, -1, -1]
 dj = [1, -1, -1, 1]
 
-def dfs(i, j, dir_s, move, ate):
-    global maxV
-    for k in range(dir_s, dir_s + 2):
-        if k == 4:
-            continue
-        ni = i + di[k]
-        nj = j + dj[k]
+def dfs(x, y, path, dir):
+    global cnt,i,j
 
-        if 0 <= ni < N and 0 <= nj < N:
-            if arr[ni][nj] in ate:
-                if visited[ni][nj] == 2:
-                    if maxV < move:
-                        maxV = move
-                        return
-                continue
+    if dir == 3 and x == i and y == j and len(path) > 2:
+        cnt = max(cnt,len(path))
 
-            visited[ni][nj] = 1
-            dfs(ni, nj, k, move + 1, ate + [arr[ni][nj]])
-            visited[ni][nj] = 0
+    if 0<=x<N and 0<=y<N and arr[x][y] not in path:
+        new_path = path + [arr[x][y]]
+
+        #직진
+        nx,ny = x+di[dir] , y+dj[dir]
+        dfs(nx,ny,new_path,dir)
+
+        #꺾기
+        if dir < 3:
+            nx, ny = x + di[dir+1], y + dj[dir+1]
+            dfs(nx,ny,new_path,dir+1)
 
 T = int(input())
 
@@ -34,14 +32,10 @@ for tc in range(1, 1 + T):
 
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    visited = [[0]*N for _ in range(N)]
-
-    maxV = -1
+    cnt = -1
 
     for i in range(N):
         for j in range(1, N):
-            visited[i][j] = 2  # 도착지
-            dfs(i, j, 0, 1, [arr[i][j]])
-            visited[i][j] = 0
+            dfs(i,j,[],0)
 
-    print(f'#{tc} {maxV}')
+    print(f'#{tc} {cnt}')
--- Problem Solving/prac.py
+++ Problem Solving/prac.py
@@ -1,48 +1,67 @@
 import sys
+from itertools import combinations
 sys.stdin = open('input.txt')
 
 T = int(input())
 
-def dfs(i,j,diggable):
-    global ans
+def find_parent(parent,x):
+    if parent[x]!=x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
 
-    ans = max(ans,visited[i][j])
+def union_parent(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
 
-    for di,dj in [[1,0],[-1,0],[0,1],[0,-1]]:
-        ni,nj = i+di, j+dj
-        if 0<=ni<N and 0<=nj<N and visited[ni][nj] == 0:
-            if arr[ni][nj] < arr[i][j]:
-                visited[ni][nj] = visited[i][j] + 1
-                dfs(ni,nj,diggable)
-                visited[ni][nj] = 0
-            elif diggable > 0 and arr[ni][nj] - K < arr[i][j]:
-                tmp = arr[ni][nj]
-                visited[ni][nj] = visited[i][j] + 1
-                arr[ni][nj] = arr[i][j] - 1
-                dfs(ni,nj,diggable-1)
-                visited[ni][nj] = 0
-                arr[ni][nj] = tmp
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 
-for tc in range(1,T+1):
-    N,K = map(int,input().split())
 
-    arr = [list(map(int,input().split())) for _ in range(N)]
+for tc in range(1,1+T):
+    N = int(input())
 
-    visited = [[0]*N for _ in range(N)]
-
-    ans = 0
-
-    top_num = 0
+    parent = [0]*(N)
 
     for i in range(N):
-        top_num = max(top_num, max(arr[i]))
+        parent[i] = i
+
+    x = list(map(int,input().split()))
+    y = list(map(int,input().split()))
+
+    E = float(input())
+
+    tmp = []
 
     for i in range(N):
-        for j in range(N):
-            if arr[i][j] == top_num:
-                visited[i][j] = 1
-                dfs(i,j,1)
-                visited[i][j] = 0
+        tmp.append((i,x[i],y[i]))
 
-    print(f'#{tc} {ans}')
+    combs = list(combinations(tmp,2))
+
+    edges = []
+
+    for comb in combs:
+        L = (comb[0][1]-comb[1][1])**2 + (comb[0][2]-comb[1][2])**2
+        w = E * L
+        edges.append((w,comb[0][0],comb[1][0]))
+
+    edges.sort()
+
+    result = 0
+    for edge in edges:
+        a,b = edge[1],edge[2]
+
+        if find_parent(parent,a) != find_parent(parent,b):
+            union_parent(parent,a,b)
+            result += edge[0]
+    print(f'#{tc} {int(result)}')
+
+
+
+
+
+
+
+
